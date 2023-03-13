bfd589052d4c9b2e33e2ba2c8177048fbe07268c
Arbi Velaj <arbi.velaj@gmail.com>
2023-03-13 10:05:31 +0100

    first commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,28 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:home]
   def home
+
+    # flash[:alert] = "you have successfully signed up"
   end
+
+  def contacts
+
+  end
+
+
+  # POST /restauants/:restaurant_id/reviews
+
+  # def create
+  #   @restaurant = Restaurant.find
+  #   @review = Review.new
+  #   @review.restaurant = @restaurant
+  # end
+
+  # # POST users/:user_id/restaurants
+  # # POST /restaurants
+
+  # def create
+  #   @restaurant = Restaurant.find
+  #   @restaurant.user = current_user
+  # end
 end
